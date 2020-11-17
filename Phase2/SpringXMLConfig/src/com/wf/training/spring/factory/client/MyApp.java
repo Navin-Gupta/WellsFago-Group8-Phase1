c15705752b744fff7b682004fce13815a50c260f
Navin-Gupta <46860917+Navin-Gupta@users.noreply.github.com>
2020-11-17 17:14:18 +0530

    Add files via upload

--- Phase2/SpringXMLConfig/src/com/wf/training/spring/factory/client/MyApp.java
+++ Phase2/SpringXMLConfig/src/com/wf/training/spring/factory/client/MyApp.java
@@ -11,15 +11,26 @@ public class MyApp {
 	public static void main(String[] args) {
 		
 		// Create a container (bean factory) based on XML file (config)
+		// all singleton beans are created here..
 		ClassPathXmlApplicationContext context =
 				new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		// demand / retrieve the beans from factory
 		// MessageService service = context.getBean(MessageService.class);
 		// MessageService service = context.getBean(EmailService.class);
+		// prototype beans are created when they are demanded
 		MessageService service = context.getBean("email", MessageService.class);
 		String ack = service.sendMessage("Hello All", "someone");
 		System.out.println(ack);
+		System.out.println("\n--------------------\n");
+		
+		MessageService servicePro = context.getBean("email", MessageService.class);
+		ack = servicePro.sendMessage("Hello All by another instance", "someone");
+		System.out.println(ack);
+		
+		// close the container(factory)
+		context.close();
+		
 	}
 
 }
--- Phase2/SpringXMLConfig/src/com/wf/training/spring/factory/service/EmailService.java
+++ Phase2/SpringXMLConfig/src/com/wf/training/spring/factory/service/EmailService.java
@@ -12,26 +12,38 @@ public class EmailService implements MessageService{
 		this.fortuneService = fortuneService;
 	}*/
 	
+	public EmailService() {
+		System.out.println("Email Bean initiated...");
+	}
+	
 	// getting dependency through setter method
-	
-	
-	
 	public void setFortuneService(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 	}
-	
-	
+
 
 	public void setSender(String sender) {
 		this.sender = sender;
 	}
 
+	
+	// can have any access modifiers
+	// can have any return type, but void is recommended
+	// can have any name
+	// must not have any param
+	private void myInit() {
+		System.out.println("Bean prepared to be used...");
+	}
 
 	@Override
 	public String sendMessage(String message, String to) {
 		return "Email sent : " + message + "[To:" + to + "]" +
 				"\nSent By :" + this.sender +
 				"\n" + this.fortuneService.dailyFortune();	
+	}
+	
+	private void myCleanUp() {
+		System.out.println("Bean ready to be destroyed...");
 	}
 
 }
