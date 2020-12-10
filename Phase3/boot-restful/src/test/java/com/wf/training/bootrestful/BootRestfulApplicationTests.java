031293bdac362b83f367d64a17012fba2b464550
Navin-Gupta <46860917+Navin-Gupta@users.noreply.github.com>
2020-12-10 20:18:29 +0530

    Add files via upload

---
+++ Phase3/boot-restful/src/test/java/com/wf/training/bootrestful/BootRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wf.training.bootrestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
